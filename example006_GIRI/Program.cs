5ffbe24d01877d4332fa44911566c86da93e9e57
Григорий <zeronfmss@gmail.com>
2023-11-19 15:35:48 +0300

    exemple006

--- example006_GIRI/Program.cs
+++ example006_GIRI/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 1;
+int c = 1;
+int d = 1;
+int e = 1;
+
+int max = a;
+
+if (a> max) max = a;
+if (b> max) max = b;
+if (c> max) max = c;
+if (d> max) max = d;
+if (e> max) max = e;
+
+Console.Write("Max = ");
+Console.Write(max);
