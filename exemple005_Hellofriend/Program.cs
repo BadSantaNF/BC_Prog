3fb2cf31f0b7e9296fe37d5d0d6ecc557fa2681a
Григорий <zeronfmss@gmail.com>
2023-11-19 15:16:21 +0300

    exemple005

--- exemple005_Hellofriend/Program.cs
+++ exemple005_Hellofriend/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Hello этоже Маша ");
+}
+else
+{
+    Console.Write("Ой иди ты, ТЫ не Маша это печально");
+    Console.WriteLine(username);
+}
