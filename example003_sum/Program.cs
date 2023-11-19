5c8f8f2eb33103902880cc326c232fc840bae136
Григорий <zeronfmss@gmail.com>
2023-11-19 14:54:35 +0300

    изсенения в сумме генератор чисел

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,5 +1,13 @@
-﻿int numberA = 3;
-int numberB = 525;
+﻿int numberA = new Random().Next(1,10);//12345.....9\
+Console.Write("Первое число :");
+
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("Второе число :");
+
+Console.WriteLine(numberB);
+
+Console.Write("Сумма :");
 int result = numberA + numberB;
 Console.WriteLine (result);
 
