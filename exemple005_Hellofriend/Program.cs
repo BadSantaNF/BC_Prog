defbfcc777b62da492a5d5166f2065ad3f2ae08c
Григорий <zeronfmss@gmail.com>
2023-11-19 18:50:52 +0300

    exemple005i

--- exemple005_Hellofriend/Program.cs
+++ exemple005_Hellofriend/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.WriteLine("введите имя пользователя");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Hello этоже Маша ");
 }
 else
 {
-    Console.Write("Ой иди ты, ТЫ не Маша это печально");
+    Console.Write("Ой иди ты, ТЫ не Маша это печально ");
     Console.WriteLine(username);
 }
+
